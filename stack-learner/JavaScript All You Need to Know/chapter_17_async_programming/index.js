f14c0f8137a4eca41b9550ffa043a856ab653552
tanvir8905 <tanvir8905.com>
2022-12-28 21:07:11 +0600

    Async Await in Javascript

--- stack-learner/JavaScript All You Need to Know/chapter_17_async_programming/index.js
+++ stack-learner/JavaScript All You Need to Know/chapter_17_async_programming/index.js
@@ -336,3 +336,42 @@
 
 // Promise.race(promiseArr)
 //     .then(v => console.log(v))
+
+//! 190. Async Await in Javascript
+// function myPromise(){
+//     return Promise.resolve('Test Value')
+// }
+// console.log(myPromise());
+
+// async function test(){
+//     return 'Test';
+// }
+// test().then(v => alert(v))
+
+// let p1 = new Promise(resolve => {
+//     setTimeout(resolve, 5000, 'Test Value')
+// })
+// async function myAsyncFunc(){
+//     // p1.then(v => console.log(v))
+//     let v = await p1
+//     console.log(v);
+// }
+// myAsyncFunc()
+
+// async function fetchData() {
+//     try{
+//         let res = await fetch('https://jsonplaceholder.typicode.com/users')
+//         let data = await res.json()
+//         let names = data.map(v => v.name)
+//         console.log(names);
+//     }catch(e){
+//         console.log(e.message);
+//     }
+// }
+// fetchData()
+
+// let promises = [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3), Promise.resolve(4)]
+// async function promiseAll(){
+//     let result = await Promise.all(promises)
+//     console.log(result);
+// }
