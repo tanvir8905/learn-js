2bc63c22f8a406b6d05278eb4630429cd8dc8e67
tanvir8905 <tanvir8905.com>
2022-12-28 20:08:51 +0600

    Promise API in Javascript

--- stack-learner/JavaScript All You Need to Know/chapter_17_async_programming/index.js
+++ stack-learner/JavaScript All You Need to Know/chapter_17_async_programming/index.js
@@ -294,10 +294,45 @@
 //         })
 
 //! 189. Promise API in Javascript
-const delay = s => new Promise((resolve) => setTimeout(resolve, s * 1000))
+// const delay = s => new Promise((resolve) => setTimeout(resolve, s * 1000))
 
-delay(2).then(() => console.log('2 seconds delay'))
-delay(1).then(() => console.log('1 seconds delay'))
-delay(3).then(() => console.log('3 seconds delay'))
-delay(2).then(() => console.log('2 seconds delay'))
-delay(5).then(() => console.log('5 seconds delay'))
+// delay(2).then(() => console.log('2 seconds delay'))
+// delay(1).then(() => console.log('1 seconds delay'))
+// delay(3).then(() => console.log('3 seconds delay'))
+// delay(2).then(() => console.log('2 seconds delay'))
+// delay(5).then(() => console.log('5 seconds delay'))
+
+// let p1 = Promise.resolve('Test')
+// p1.then((v) => console.log(v))
+
+// let p2 = Promise.reject('REJECT')
+// p2.catch((e) => console.log(e))
+
+// let p1 = Promise.resolve('One')
+// let p2 = Promise.resolve('Two')
+// let p3 = Promise.resolve('Three')
+
+// let promiseArr = [p1, p2, p3]
+// Promise.all(promiseArr)
+//     .then(arr => {
+//         console.log(arr);
+//     })
+
+// let p1 = new Promise( resolve =>{
+//     setTimeout(resolve, 5000, 'One')
+// })
+
+// let p2 = new Promise( resolve =>{
+//     setTimeout(resolve, 3000, 'Two')
+// })
+
+// let p3 = new Promise( resolve =>{
+//     setTimeout(resolve, 4000, 'Three')
+// })
+
+// let promiseArr = [p1, p2, p3]
+// Promise.all(promiseArr)
+//     .then(arr => console.log(arr))
+
+// Promise.race(promiseArr)
+//     .then(v => console.log(v))
