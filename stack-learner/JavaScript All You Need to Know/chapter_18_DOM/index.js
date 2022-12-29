b89852977ad5155c13ef39137ffdcb4f90e2a9eb
tanvir8905 <tanvir8905.com>
2022-12-29 12:52:43 +0600

    Document Object in Javascript

--- stack-learner/JavaScript All You Need to Know/chapter_18_DOM/index.js
+++ stack-learner/JavaScript All You Need to Know/chapter_18_DOM/index.js
@@ -2,10 +2,14 @@
 // ? DOM - Document Object Model
 // 1. DOM is a tree data structure. Just an API to interact with the HTML document and change it later on demand. 
 //* 2. 4 types of node. 1. Element 2. Attributes 3. Text 4. Comment
-//* 3. HTML -> Node -> DOM Tree -> JS code -> DOM Manipulation(Tree Update) -> Show on Browser
+//* 3. HTML -> Node -> DOM Tree(JS object) -> JS DOM Manipulation(Tree Update) -> Show on Browser
 
 //! 195. Window Object in Javascript
 //? type of window -> object
 // window.location, window.setTimeout, window.document, window.Math
 // var accessible in window object but let not accessible in window object. Ex: location, setTimeout, document, Math
 // window object is accessible directly from browser
+
+//! 196. Document Object in Javascript
+// html = document
+// document.body, document.URL, document.head
