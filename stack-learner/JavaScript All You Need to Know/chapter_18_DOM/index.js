019b6d59c25b1d03d55c80c69d0937bfd43c4e5f
tanvir8905 <tanvir8905.com>
2022-12-29 08:16:19 +0600

    Introduction to DOM

--- stack-learner/JavaScript All You Need to Know/chapter_18_DOM/index.js
+++ stack-learner/JavaScript All You Need to Know/chapter_18_DOM/index.js
@@ -1,6 +1,5 @@
 //! 194. Introduction to DOM
-// ? DOM - document object model
-// 1. Just an API to interact with the HTML document and change it later on demand
-// 2. Maintain Tree data structure. 
-//* 3. 4 types of node. 1. Element, 2. Attributes, 3. Text, 4. Comment
-// 4. 
+// ? DOM - Document Object Model
+// 1. DOM is a tree data structure. Just an API to interact with the HTML document and change it later on demand. 
+//* 2. 4 types of node. 1. Element 2. Attributes 3. Text 4. Comment
+//? 3. HTML -> Node -> DOM Tree -> JS code -> DOM Manipulation(Tree Update) -> Show on Browser
